59624e98dc857bcf9176a98bdf41922b958a2b0e
apanchal2@gmail.com <apanchal2@gmail.com>
2019-06-12 16:47:53 -0400

    fixed beans.xml

---
+++ Project2/src/main/java/com/greenbank/controllers/LoginController.java
@@ -0,0 +1,37 @@
+package com.greenbank.controllers;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.greenbank.beans.UserInfo;
+import com.greenbank.data.UserInfoDAO;
+
+
+@Controller
+@RequestMapping(value="/login")
+public class LoginController {
+	@Autowired
+	private UserInfoDAO ud;
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public String goLogin(HttpSession session) {
+		if(session.getAttribute("user")!=null) {
+			return "redirect:home";
+		}
+		return "static/login.html";
+	}
+	@PostMapping
+	public String login(String username, String password, HttpSession session) {
+		UserInfo u = ud.getUser(username, password);
+		if(u!=null) {
+			session.setAttribute("user", u);
+			return "redirect:home";
+		}
+		return "redirect:login";
+	}
+}
