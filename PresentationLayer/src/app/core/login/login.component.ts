8590a08117c8f515f149b7f78badeca2cf1945c6
Andrey <andreySuvorov94@gmail.com>
2019-06-13 11:26:01 -0400

    login service

--- PresentationLayer/src/app/core/login/login.component.ts
+++ PresentationLayer/src/app/core/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from './login.service';
+// import { UserInfo } from 'src/app/shared/beans/userinfo';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  value1 = '';
+  value2 = '';
+ // public dude: UserInfo;
 
-  constructor() { }
 
+  constructor(private loginService: LoginService ) {}
   ngOnInit() {
   }
 
+  onEnter1(value1: string) {
+     this.value1 = value1; }
+
+  onEnter2(value2: string) {
+     this.value2 = value2; }
+
+
+     
+  // login(){
+  //   this.loginService.login(this.value1, this.value2).subscribe(
+  //     resp => {
+  //       this.dude =  resp;
+  //       console.log(this.dude);
+  //     });
+
+  // }
+
+  register(){
+
+  }
 }
