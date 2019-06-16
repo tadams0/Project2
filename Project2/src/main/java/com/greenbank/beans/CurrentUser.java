a12f4acec16447239e42ded4c34f48ac5b777d58
Andrey <andreySuvorov94@gmail.com>
2019-06-16 17:55:46 -0400

    updating changes to personal branch

---
+++ Project2/src/main/java/com/greenbank/beans/CurrentUser.java
@@ -0,0 +1,60 @@
+package com.greenbank.beans;
+
+//used to help map the response on front end
+public class CurrentUser {
+
+	Employee employee;
+	Customer customer;
+	
+	public CurrentUser() {
+		super();
+	}
+	
+	public Employee getEmployee() {
+		return employee;
+	}
+	public void setEmployee(Employee employee) {
+		this.employee = employee;
+	}
+	public Customer getCustomer() {
+		return customer;
+	}
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((customer == null) ? 0 : customer.hashCode());
+		result = prime * result + ((employee == null) ? 0 : employee.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CurrentUser other = (CurrentUser) obj;
+		if (customer == null) {
+			if (other.customer != null)
+				return false;
+		} else if (!customer.equals(other.customer))
+			return false;
+		if (employee == null) {
+			if (other.employee != null)
+				return false;
+		} else if (!employee.equals(other.employee))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "CurrentUser [employee=" + employee + ", customer=" + customer + "]";
+	}
+	
+	
+}
